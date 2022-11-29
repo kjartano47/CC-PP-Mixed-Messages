5e2c1b378917db5572e0929eca0456c95227105b
kjartano47 <kjartano97@gmail.com>
2022-11-29 18:25:58 +0000

    Added proverb strings

---
+++ main.js
@@ -0,0 +1,16 @@
+//Proverb generator with three parts
+/*step 1: create dummy text. three arrays for the three parts of the proverb */
+
+const arr1 = ["Never has the ", "The ", "Soon the ", "Sometimes"];
+const arr2 = ["time ", "apple", "gators"];
+const arr3 = ["of the forest ", "of life "];
+const arr4 = [
+	"compells the old beggar",
+	"distracts the singing lady",
+	"destroys the confidence of man",
+	"exceeds the expectation of god",
+];
+
+/*step 2: create a combiner that randomly selects one string from each of the three arrays */
+
+/*step 3: print the results*/
